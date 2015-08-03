1c422668225c43284fe4d41fc1bf88c1cc2ad306
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-08-03 18:28:56 -0500

    Site updated at 2015-08-03 23:28:56 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -30,8 +30,8 @@ function getNav() {
 //       $(section).addClass(count);
 //     });
 //   }
-  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
-}
+//   if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
+// }
 
 function testFeatures() {
   var features = ['maskImage'];
